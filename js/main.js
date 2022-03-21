c408dc885021c37ef063d5a5f732c98a991703b0
muhammadsodiq07 <muhammadodiqkoziyev1@gmail.com>
2022-03-21 14:20:24 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 let pizzas = [{
-    imageUrl: '../images/pizza1.png',
-    desc: 'Pepperoni',
-    price: 2.23,
-    id: 1
-  },
-  {
-    imageUrl: '../images/pizza2.jpg',
-    desc: 'Cheesy',
-    price: 5.99,
-    id: 2
-  },
-  {
-    imageUrl: '../images/pizza3.jpg',
-    desc: 'Margarita',
-    price: 7.48,
-    id: 3
-  },
-  {
-    imageUrl: '../images/pizza4.jpg',
-    desc: 'Hawaiian',
-    price: 9.32,
-    id: 4
-  },
+        imageUrl: '../images/pizza1.png',
+        desc: 'Pepperoni',
+        price: 2.23,
+        id: 1
+    },
+    {
+        imageUrl: '../images/pizza2.jpg',
+        desc: 'Cheesy',
+        price: 5.99,
+        id: 2
+    },
+    {
+        imageUrl: '../images/pizza3.jpg',
+        desc: 'Margarita',
+        price: 7.48,
+        id: 3
+    },
+    {
+        imageUrl: '../images/pizza4.jpg',
+        desc: 'Hawaiian',
+        price: 9.32,
+        id: 4
+    },
 
 ]
 
@@ -30,9 +30,9 @@ let ulList = document.querySelector('.pizza__list');
 
 for (let i = 0; i < pizzas.length; i++) {
 
-  let li = document.createElement("li");
-  li.classList.add('pizza__item');
-  li.innerHTML = `
+    let li = document.createElement("li");
+    li.classList.add('pizza__item');
+    li.innerHTML = `
   <div class="bgcolor d-flex">
   <div class="pizza__imgdiv">
   <img class="pizza__img" src="${pizzas[i].imageUrl}" alt="">
@@ -43,19 +43,13 @@ for (let i = 0; i < pizzas.length; i++) {
       </p>
       <p class="pizza__price">$${pizzas[i].price}</p>
       <div class="pizza__btnbox">
-      <button class="btn pizza__btn" id = ${pizzas[i].id}>
-      Add to Cart
-      </button>
+      <button onclick="getAdd(${pizzas[i].id})" class="btn pizza__btn">Add to Card</button>
       </div>
       </div>
       </div>
       `
-  ulList.appendChild(li);
+    ulList.appendChild(li);
 }
-
-
-let arrNew = [];
-let arrPrice = [];
 const elBtnAdd = document.querySelectorAll('.btn');
 const elListCart = document.querySelector('.cart__list')
 const elSub = document.querySelector('.title-pr');
@@ -63,112 +57,163 @@ const elTax = document.querySelector('.tax-pr');
 const elTotal = document.querySelector('.total-pr');
 
 
-let sub = 0;
-let tex = 0;
-let total = 0;
 
-for (let i = 0; i < elBtnAdd.length; i++) {
-  elBtnAdd[i].addEventListener('click', () => {
-    arrNew[arrNew.length] = pizzas[i];
-    arrPrice[arrPrice.length] = pizzas[i].price;
-    
-    sub += pizzas[i].price;
-    tax = sub * 10 / 100;
-    total = (sub + tax);
 
-    let li = document.createElement('li');
-    li.className =("cart__item animation");
-    li.innerHTML = `<div class="bgcolor bgcolor2 d-flex mb-5">
-    <div class="pizza__imgdiv">
-    <img class="pizza__img" src="${arrNew[i].imageUrl}" alt="">
-      </div>
-      <div class="pizza__pl">
-      <p class="pizza__name">
-        ${arrNew[i].desc}
-        </p>
-        <p class="pizza__price">$${arrNew[i].price}</p>
-        <button class="minus"  onclick='removeItem(${i})'>
-        <i class='bx bx-minus'></i>
-      </button>
-        <button class="pizza__number">1</button>
-        </div>
-        </div>
-                  `;
-    elSub.textContent = sub.toFixed(2);
-    elTax.textContent = tax.toFixed(2);
-    elTotal.textContent = total.toFixed(2);
+let newArr = [];
 
-    elListCart.appendChild(li);
-  })
-}
 
-function removeItem(index) {
-  let newArrRemove = [];
 
-  for (let i = 0; i < arrNew.length; i++) {
-    if (index != i) {
-      newArrRemove.push(arrNew[i]);
+
+function findFood(id) {
+    for (let i = 0; i < pizzas.length; i++) {
+        if (id == pizzas[i].id) {
+            return pizzas[i]
+        }
     }
-    
-  }
-  
-  arrNew = newArrRemove;
 
-  elListCart.innerHTML = "";
-  sub = 0;
-  tex = 0;
-  total = 0;
-
-  for (let i = 0; i < arrNew.length; i++) {
-    let li = document.createElement("li");
-    li.className =("cart__item");
-    li.innerHTML = `<div class="bgcolor bgcolor2 d-flex mb-5">
-    <div class="pizza__imgdiv">
-    <img class="pizza__img" src="${arrNew[i].imageUrl}" alt="">
-      </div>
-      <div class="pizza__pl">
-      <p class="pizza__name">
-        ${arrNew[i].desc}
-        </p>
-        <p class="pizza__price">$${arrNew[i].price}</p>
-        <button class="minus"  onclick='removeItem(${i})'>
-        <i class='bx bx-minus'></i>
-        </button>
-        <button class="pizza__number">1</button>
-        </div>
-        </div>
-    `;
-    
-    sub += arrNew[i].price;
-    elSub.textContent = sub.toFixed(2);
-
-    tax = sub % 10 / 100;
-    elTax.textContent = tax.toFixed(2);
-    
-    total = (sub + tax);
-    elTotal.textContent = total.toFixed(2);
-
-    elListCart.appendChild(li);
-  }
-  if (arrNew.length == 0 ) {
-    sub == 0;
-    elSub.textContent = sub.toFixed(2);
-    total == 0;
-    elTotal.textContent = total.toFixed(2);
-    tex == 0;
-    elTax.textContent = tex.toFixed(2);
-  }
 }
+
+function checkArray(item) {
+
+
+    if (!newArr.length) {
+        const newFood = item
+        newFood.count = 1
+        return newArr.push(newFood)
+    }
+
+    for (let i = 0; i < newArr.length; i++) {
+        if (item.id === newArr[i].id) {
+            return newArr[i].count = newArr[i].count + 1
+        }
+    }
+
+    const newFood = item
+    newFood.count = 1
+
+    newArr.push(newFood)
+}
+
+function deleteFood(id) {
+    const arr = [];
+
+    for (let i = 0; i < newArr.length; i++) {
+        if (id === newArr[i].id) {
+            if (newArr[i].count > 1) {
+                const newFood = newArr[i]
+
+                newFood.count = newFood.count - 1;
+
+                arr.push(newFood)
+            }
+        } else {
+            arr.push(newArr[i]);
+        }
+    }
+
+    newArr = arr
+    renderCardFood()
+}
+
+function renderCardFood() {
+    elListCart.innerHTML = '';
+
+    let subtitle = 0;
+
+    for (let i = 0; i < newArr.length; i++) {
+        let li = document.createElement('li');
+        li.className = ("cart__item animation");
+        li.innerHTML = `<div class="bgcolor bgcolor2 d-flex mb-5">
+          <div class="pizza__imgdiv">
+          <img class="pizza__img" src="${newArr[i].imageUrl}" alt="">
+            </div>
+            <div class="pizza__pl">
+            <p class="pizza__name">
+              ${newArr[i].desc}
+              </p>
+              <p class="pizza__price">$${newArr[i].price}</p>
+              <button class="count-btn">${newArr[i].count}</button>
+              <div class = "btndiv">
+              <button class="push-btn" onclick="btn(${i})">+</button>
+              <button onclick = "removeButton(${i})" class="remove-btn">-</button>
+              </div>
+              </div>
+              </div>
+        `
+
+        subtitle += newArr[i].count * newArr[i].price
+        elListCart.appendChild(li);
+
+    }
+
+    let taxiPrice = subtitle * 0.1;
+    total = (subtitle + taxiPrice);
+    elTax.innerHTML = `${taxiPrice.toFixed(2)}$`;
+    elTotal.innerHTML = `${total.toFixed(2)} $`
+    elSub.innerHTML = `${subtitle.toFixed(2)} $`;
+}
+
+function btn(foodIndex) {
+    newArr[foodIndex].count = newArr[foodIndex].count + 1
+
+    renderCardFood()
+}
+
+function removeButton(foodIndex) {
+    let foundFood = newArr[foodIndex]
+    if (foundFood.count > 1) {
+        foundFood.count = foundFood.count - 1
+        return renderCardFood()
+    }
+
+    let arr = []
+
+    for (let i = 0; i < newArr.length; i++) {
+        if (newArr[i].id !== foundFood.id) {
+            arr.push(newArr[i])
+        }
+    }
+
+    newArr = arr
+
+    renderCardFood()
+}
+
+
+function getAdd(id) {
+    let foundFood = findFood(id)
+
+    checkArray(foundFood)
+
+    renderCardFood();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 
 // let arr = [5,2,1,1,3,5,5,2,2,4];
-
 // let b = [];
 // let count = arr.length;
-
 // for(let i = 0; i < count; i++) {
 //   let k = [];
 //   let f = [];
@@ -183,3 +228,8 @@ function removeItem(index) {
 //   if (k != "") b.push(k);
 // }
 // console.log(b);
+
+// let arr2 = pizzas.filter(function(pic) {
+//   if()
+// });
+// console.log(arr2);
